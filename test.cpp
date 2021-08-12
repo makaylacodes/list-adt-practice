fe99bb6f56866a5f8dd4e71b86d5448d8872cb9f
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-08-12 16:45:50 +0900

    I have created a function that inserts a new node at the end of the list

--- test.cpp
+++ test.cpp
@@ -16,12 +16,39 @@ void printList(Node* n){
         n = n->next;
     }
 }
+
+//This function will insert new node at start
 void insertFront (Node** head, int newValue) {
     Node* newNode = new Node();
     newNode -> value = newValue;
     newNode -> next = *head;
     *head = newNode;
 }
+
+//This function will insert new node at end
+void insertEnd(Node** head, int newValue){
+    //1. prepare a new node
+    Node* newNode = new Node();
+    newNode -> value = newValue;
+    newNode -> next = NULL;
+
+    //2. If linked list is empty, newNode will be head node
+    if (*head == NULL){
+        *head = newNode;
+        return;
+    }
+
+    //3. Find the last node
+    Node* last = *head;
+    while (last -> next != NULL){
+        last = last->next;
+    }
+
+    //4. Insert newNode after last node
+    last->next = newNode;
+}
+
+
 int main (){
     Node* head = new Node();
     Node* second = new Node();
@@ -35,6 +62,8 @@ int main (){
     last -> next = NULL; 
     insertFront(&head, -1);
     insertFront(&head, -2);
+
+    insertEnd(&head, 4);
     printList(head);
     return 0;
 }
