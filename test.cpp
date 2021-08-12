33b771dfe74c47b66ea013b2763061e9d3420cd6
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-08-12 16:22:52 +0900

    I have added the code for a basic list set up. Will add other branches demonstrating adding and removing elements from a list

--- test.cpp
+++ test.cpp
@@ -1,7 +1,34 @@
 #include <iomanip>
 #include<iostream>
 using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next; //this is the pointer for the next value in the list
+
+};
+
+//This function will print out all values in the list
+void printList(Node* n){ 
+    while (n != NULL){
+        cout<< n->value << endl;
+        n = n->next;
+    }
+}
+
 int main (){
-    cout << "Hello World";
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* last = new Node();
+
+    head -> value = 1;
+    head ->next = second;
+    second -> value =2;
+    second -> next = last;
+    last -> value = 3;
+    last -> next = NULL;
+
+    printList(head);
     return 0;
 }
